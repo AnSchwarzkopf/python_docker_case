32e0636a36608ef10f338fd595696f9ed4a2b438
andreasschwarzkopf <schwarzkopf.and@gmail.com>
2020-07-03 18:50:35 +0200

    Train Random Forest model on train split

--- main/main.py
+++ main/main.py
@@ -2,6 +2,7 @@ import logging
 
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 def fetchDatasets():
     '''Fetch dataset from openml provided from code snipped and returns data and target'''
@@ -14,11 +15,17 @@ def createTrainAndTestSplit(X, y, train_samples, test_size):
     logging.info("Create train and test split...")
     return train_test_split(X, y, train_size=train_samples, test_size=test_size)
 
+def trainModel(X_train, y_train):
+    logging.info("Training on train split...")
+    return RandomForestClassifier(n_jobs=-1).fit(X_train, y_train)
+
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
     X, y = fetchDatasets()
     # train samples is already provided, test size from http://yann.lecun.com/exdb/mnist/: The training set contains 60000 examples, and the test set 10000 examples.
     X_train, X_test, y_train, y_test = createTrainAndTestSplit(X, y, train_samples=60000, test_size=10000)
+
+    rfmodel = trainModel(X_train, y_train)
 
     logging.info("Done")
